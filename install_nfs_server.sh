2faa2cea34f823ac67150c081d3d9b776270c4b0
Madi-art <elswordben@gmail.com>
2023-06-04 13:20:07 +0200

    ajout du script d installation du serveur nfs

---
+++ install_nfs_server.sh
@@ -0,0 +1,26 @@
+sudo dnf install nfs-utils -y
+
+sudo sed -i '/^#Domain/s/^#//;/Domain = /s/=.*/= control-node/' /etc/idmapd.conf
+
+#export des fichiers de conf
+export host
+
+
+sudo mkdir /var/nfs/share -p
+sudo chown nobody:nobody /var/nfs/share
+
+#ajout rule d autorisation de la rule permettant a tout les reseau de se connecter a ce dossier ci
+echo "/srv/nfsshare/data  *(rw,sync,no_subtree_check,no_root_squash,insecure)" >> /etc/exports
+
+#Ajout des differentes rules et auto activation du serveur au boot
+sudo firewall-cmd --add-service={nfs,nfs3,mountd,rpc-bind} --permanent
+sudo firewall-cmd --reload
+
+sudo systemctl enable --now nfs-server rpcbind
+
+#on reload la conf nfs
+sudo exportfs -ra
+
+
+#On regarde si le fichier de conf nsf a bien été configuré
+sudo exportfs -v
